896d5cecfeb999cb9d36242792e1ff7b62ceca32
Franciscoffv <franffv0809@gmail.com>
2025-02-04 22:46:01 -0600

    semana1

---
+++ portafolioFrancisco/src/main/java/portafolio/protafolio/PortafolioFranciscoFuentesApplication.java
@@ -0,0 +1,13 @@
+package portafolio.protafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioFranciscoFuentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioFranciscoFuentesApplication.class, args);
+	}
+
+}
---
+++ portafolioFrancisco/src/test/java/portafolio/protafolio/PortafolioFranciscoFuentesApplicationTests.java
@@ -0,0 +1,13 @@
+package portafolio.protafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioFranciscoFuentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
