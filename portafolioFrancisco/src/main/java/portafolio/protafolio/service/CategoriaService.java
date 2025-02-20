936a34214a1d3549976787c1f6dd82a20b281700
Franciscoffv <franffv0809@gmail.com>
2025-02-19 23:13:20 -0600

    semana6

---
+++ portafolioFrancisco/src/main/java/portafolio/protafolio/service/CategoriaService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package portafolio.protafolio.service;
+
+/**
+ *
+ * @author franf
+ */
+import portafolio.protafolio.domain.Categoria;
+import java.util.List;
+
+public interface CategoriaService {
+    
+    // Se obtiene un listado de categorias en un List
+    public List<Categoria> getCategorias(boolean activos);
+ 
+}
