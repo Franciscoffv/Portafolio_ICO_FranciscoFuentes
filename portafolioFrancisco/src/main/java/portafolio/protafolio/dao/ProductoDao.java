677e8e4beb62cedec8df7983fcf62435afbced70
Franciscoffv <franffv0809@gmail.com>
2025-03-12 14:06:36 -0600

    semana8

---
+++ portafolioFrancisco/src/main/java/portafolio/protafolio/dao/ProductoDao.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package portafolio.protafolio.dao;
+
+import portafolio.protafolio.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoDao extends JpaRepository <Producto, Long> {
+    
+}
